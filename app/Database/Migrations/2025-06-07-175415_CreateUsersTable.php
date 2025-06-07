44e7df4ba881bb29d98c9f071fcd7a6e06568e0c
hasbi <ashshiddiqihasbi@gmail.com>
2025-06-08 01:10:46 +0700

    2nd commit

---
+++ app/Database/Migrations/2025-06-07-175415_CreateUsersTable.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'is_active' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 1,
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+                'null' => true,
+            ],
+            'role' => [
+                'type' => 'ENUM',
+                'constraint' => ['admin', 'technician', 'customer'],
+                'default' => 'customer',
+            ],
+            'last_login' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addUniqueKey('username');
+        $this->forge->addUniqueKey('email');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
